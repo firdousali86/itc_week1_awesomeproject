4cad0ae228b3880ddd3d2352ee7e440de81c9f1e
Firdous Ali <firdous.ali.tp@gmail.com>
2023-03-29 13:34:37 +0500

    remove dead code

--- App.tsx
+++ App.tsx
@@ -25,46 +25,8 @@ import SubCom from './SubCom';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const _onPressButton = () => {
-    Alert.alert('You tapped the button!');
-  };
 
   const HomeScreen = ({navigation, route}) => {
     useEffect(() => {
@@ -202,49 +164,6 @@ function App(): JSX.Element {
       </Stack.Navigator>
     </NavigationContainer>
   );
-
-  // return (
-  //   <NavigationContainer>
-  //     <SafeAreaView style={backgroundStyle}>
-  //       <StatusBar
-  //         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-  //         backgroundColor={backgroundStyle.backgroundColor}
-  //       />
-  //       <ScrollView
-  //         contentInsetAdjustmentBehavior="automatic"
-  //         style={backgroundStyle}>
-  //         <Header />
-  //         <TouchableNativeFeedback
-  //           onPress={_onPressButton}
-  //           style={{backgroundColor: 'red', marginHorizontal: 20}}>
-  //           <View>
-  //             <Text>test button</Text>
-  //           </View>
-  //         </TouchableNativeFeedback>
-  //         <Button title={'test button'} />
-  //         <View
-  //           style={{
-  //             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-  //           }}>
-  //           <Section title="Step One">
-  //             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-  //             screen and then come back to see your edits.
-  //           </Section>
-  //           <Section title="See Your Changes">
-  //             <ReloadInstructions />
-  //           </Section>
-  //           <Section title="Debug">
-  //             <DebugInstructions />
-  //           </Section>
-  //           <Section title="Learn More">
-  //             Read the docs to discover what to do next:
-  //           </Section>
-  //           <LearnMoreLinks />
-  //         </View>
-  //       </ScrollView>
-  //     </SafeAreaView>
-  //   </NavigationContainer>
-  // );
 }
 
 const styles = StyleSheet.create({
